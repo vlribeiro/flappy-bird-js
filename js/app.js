14a6a431b3db38dd07623919ef29a8f38eee3c49
Vinicius Ribeiro <vl.ribeiro3@gmail.com>
2020-04-19 02:00:11 -0300

    draw and animate bird

--- js/app.js
+++ js/app.js
@@ -12,26 +12,48 @@ let flappy = (function () {
     },
   };
 
+  // Frame control
+  const frameControl = {
+    currentFrame: 0,
+    update() {
+      this.currentFrame = (this.currentFrame + 1) % Number.MAX_SAFE_INTEGER;
+    },
+  };
+
+  // Util
+  const Util = {
+    range(start, end) {
+      return Array(end - start + 1)
+        .fill()
+        .map((_, idx) => start + idx);
+    },
+  };
+
   // Flappy Bird
   const flappyBird = {
     x: 10,
     y: 50,
-    height: 24,
-    width: 36,
+    diameter: 36,
     speed: 0,
     gravity: 0.25,
     jump: 4.6,
     alreadyCollided: false,
+    wingPositions: [],
     init() {
       this.x = 10;
       this.y = 50;
+      this.width = this.diameter;
       this.speed = 0;
       this.alreadyCollided = false;
+      this.wingPositions = Util.range(-10, 10).concat(
+        Util.range(-10, 10).reverse()
+      );
     },
     update() {
       if (collides(flappyBird, floor)) {
         if (!this.alreadyCollided) {
           audio.play("hit-1");
+          floor.speed = 0;
           this.alreadyCollided = true;
         }
 
@@ -46,9 +68,9 @@ let flappy = (function () {
     draw() {
       context.beginPath();
       context.arc(
-        this.x + this.width / 2,
-        this.y + this.height / 2,
-        this.width / 2,
+        this.x + this.diameter / 2,
+        this.y + this.diameter / 2,
+        this.diameter / 2,
         0,
         2 * Math.PI
       );
@@ -56,6 +78,66 @@ let flappy = (function () {
       context.fillStyle = "#f6e05e";
       context.lineWidth = 4;
       context.strokeStyle = "#ecc94b";
+
+      context.fill();
+      context.stroke();
+
+      context.beginPath();
+      context.arc(
+        this.x + this.diameter - 10,
+        this.y + this.diameter / 2 - 6,
+        2,
+        0,
+        2 * Math.PI
+      );
+
+      context.fillStyle = "#2d3748";
+      context.lineWidth = 2;
+      context.strokeStyle = "#f7fafc";
+
+      context.fill();
+      context.stroke();
+
+      context.beginPath();
+      context.moveTo(this.x + 6, this.y + this.diameter / 2);
+
+      const frameToDraw = frameControl.currentFrame % this.wingPositions.length;
+
+      context.bezierCurveTo(
+        this.x + this.diameter / 2 - 4,
+        this.y + this.diameter / 2 - this.wingPositions[frameToDraw],
+        this.x + this.diameter / 2 + 4,
+        this.y + this.diameter / 2 - this.wingPositions[frameToDraw],
+        this.x + this.diameter - 6,
+        this.y + this.diameter / 2
+      );
+
+      context.fillStyle = "#f6e05e";
+      context.lineWidth = 4;
+      context.strokeStyle = "#ecc94b";
+
+      context.stroke();
+      context.fill();
+
+      context.beginPath();
+
+      context.fillStyle = "#ed8936";
+      context.lineWidth = 4;
+      context.strokeStyle = "#dd6b20";
+
+      const peckOffset = 4;
+      context.moveTo(
+        this.x + this.diameter,
+        this.y + this.diameter / 2 - peckOffset
+      );
+      context.bezierCurveTo(
+        this.x + this.diameter + 10,
+        this.y + this.diameter / 2 - this.wingPositions[frameToDraw] / 5,
+        this.x + this.diameter + 10,
+        this.y + this.diameter / 2 - this.wingPositions[frameToDraw] / 5,
+        this.x + this.diameter,
+        this.y + this.diameter / 2 + peckOffset
+      );
 
       context.fill();
       context.stroke();
@@ -97,9 +179,38 @@ let flappy = (function () {
     width: 0,
     x: 0,
     y: 0,
+    patternContext: null,
+    grassPerScreen: 40,
+    grassHeight: 8,
+    speed: 0.2,
     init() {
-      this.width = canvas.width;
+      this.speed = 0.2;
+      this.width = canvas.width * 2;
       this.y = canvas.height - this.height;
+      this.createPattern();
+    },
+    createPattern() {
+      const patternCanvas = document.createElement("canvas");
+      this.patternContext = patternCanvas.getContext("2d");
+
+      patternCanvas.width = this.width / (2 * this.grassPerScreen);
+      patternCanvas.height = this.grassHeight;
+
+      this.patternContext.fillStyle = "#2f855a";
+      this.patternContext.fillRect(
+        0,
+        0,
+        patternCanvas.width,
+        patternCanvas.height
+      );
+
+      this.patternContext.fillStyle = "#48bb78";
+      this.patternContext.fillRect(
+        0,
+        1,
+        patternCanvas.width / 2,
+        patternCanvas.height - 2
+      );
     },
     draw() {
       const gradient = context.createLinearGradient(
@@ -112,11 +223,24 @@ let flappy = (function () {
       gradient.addColorStop(1, "#22543d");
 
       context.fillStyle = gradient;
-      context.lineWidth = 6;
-      context.strokeStyle = "#2f855a";
-
-      context.strokeRect(this.x, this.y, this.width, this.height);
       context.fillRect(this.x, this.y, this.width, this.height);
+
+      this.patternContext.translate(this.x, 0);
+      const pattern = context.createPattern(
+        this.patternContext.canvas,
+        "repeat"
+      );
+
+      pattern.setTransform(new DOMMatrix().translateSelf(this.x, 0, 0));
+
+      context.fillStyle = pattern;
+      context.fillRect(this.x, this.y, this.width, this.grassHeight);
+    },
+    update() {
+      this.x = (this.x - this.speed) % (this.width / 2);
+    },
+    increaseSpeed() {
+      this.speed += 0.2;
     },
   };
 
@@ -148,6 +272,9 @@ let flappy = (function () {
         floor.init();
         bg.init();
       },
+      update() {
+        floor.update();
+      },
       draw() {
         bg.draw();
         floor.draw();
@@ -159,6 +286,9 @@ let flappy = (function () {
       },
     },
     game: {
+      init() {
+        floor.increaseSpeed();
+      },
       draw() {
         bg.draw();
         floor.draw();
@@ -166,6 +296,7 @@ let flappy = (function () {
       },
       update() {
         flappyBird.update();
+        floor.update();
       },
       click() {
         flappyBird.pula();
@@ -189,6 +320,7 @@ let flappy = (function () {
   };
 
   let loop = function () {
+    frameControl.update();
     screens.active.draw();
 
     if (screens.active.update) screens.active.update();
